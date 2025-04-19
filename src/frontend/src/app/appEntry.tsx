c874ea1db5cce949462dc3ddce1c5fd423903371
webHikari <q5503212@gmail.com>
2025-04-19 12:21:43 +0300

    Mantine now works, add theme baseline

--- src/frontend/src/app/appEntry.tsx
+++ src/frontend/src/app/appEntry.tsx
@@ -1,17 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "./providers/Router.provider";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
+
 
 // global styles
+import '@mantine/core/styles.css';
 import "./styles/layout.css";
 import "./styles/colors.css";
 import "./styles/reset.css";
 import "./styles/transitions.css";
 
+
+
+
+const theme = createTheme({
+  primaryColor: "blue",
+  fontFamily: "Inter, sans-serif",
+  defaultRadius: "md",
+});
+
+
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider 
+      defaultColorScheme="light"
+      theme={theme}
+    >
       <RouterProvider />
     </MantineProvider>
   </StrictMode>,
