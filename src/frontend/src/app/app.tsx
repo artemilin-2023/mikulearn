832ae351d3e930f50561a058edebb50c993dc3f0
movpushmov <gigolaev.edward@gmail.com>
2025-04-19 12:28:32 +0300

    feat: mantine theme & no cssinjs

--- src/frontend/src/app/app.tsx
+++ src/frontend/src/app/app.tsx
@@ -1,9 +1,15 @@
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 import { RoutesView } from '@pages';
+
+const theme = createTheme({
+  primaryColor: 'blue',
+  fontFamily: 'Inter, sans-serif',
+  defaultRadius: 'md',
+});
 
 export function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme} defaultColorScheme="light">
       <RoutesView />
     </MantineProvider>
   );
