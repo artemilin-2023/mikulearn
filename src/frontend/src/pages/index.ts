3d83564b420e40a7dcfcc9c8db178b719db963cd
4310V343k <56449763+4310V343k@users.noreply.github.com>
2025-04-20 07:47:30 +0300

    fuck you

--- src/frontend/src/pages/index.ts
+++ src/frontend/src/pages/index.ts
@@ -1,5 +1,4 @@
 export * from './home/home-page';
-export * from './dashboard/dashboard-page';
 export * from './not-found/not-found-page';
 export * from './sign-in/sign-in-page';
 export * from './sign-up/sign-up-page';
