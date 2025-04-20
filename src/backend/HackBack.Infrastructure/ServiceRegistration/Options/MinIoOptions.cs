4a74942d3053941355cb49024ec912241a408adb
artemilin-2023 <136802420+artemilin-2023@users.noreply.github.com>
2025-04-20 11:25:12 +0300

    попытка починить minio?

--- src/backend/HackBack.Infrastructure/ServiceRegistration/Options/MinIoOptions.cs
+++ src/backend/HackBack.Infrastructure/ServiceRegistration/Options/MinIoOptions.cs
@@ -2,7 +2,6 @@
 {
     internal class MinIoOptions
     {
-        public string Endpoint { get; set; } = default!;
         public string AccessKey { get; set; } = default!;
         public string SecretKey { get; set; } = default!;
         public string BucketName { get; set; } = default!;
